87284e0abc757840827aee6bb32f1db0b93aee43
JivkoJelev91 <zhelev.zhivko.1991@gmail.com>
2019-03-19 17:25:05 +0200

    react drag and drop example

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { Draggable, Droppable } from 'react-drag-and-drop'
+import './App.css';
+
+const boxes = [0,1,2,3]; // Depends of how many boxes you have
+const src = 'https://unsplash.it/200/200?image=1074';
+const droppableParent = {
+  display: 'flex',
+  justifyContent: 'space-around',
+  width: '100%'
+};
+
+class App extends Component {
+
+  state = {
+    dragElement: '',
+    indexImg: 0,
+  }
+
+  onDrop = (data, e) => {
+    let index = +e.target.children[0].getAttribute('index');
+    this.setState({
+      dragElement: data.img,
+      indexImg: index
+    })
+  }
+
+  render() {
+    return(
+        <div className="App">
+          <div style={droppableParent}>
+            {boxes.map((box, key) => {
+                return (
+                  this.state.indexImg === key ?
+                  <Draggable 
+                  key={key}
+                  type="img" 
+                  data={this.src}>
+                  <div className="draggedImg"><img src={src} alt="img"/></div>
+                  </Draggable>
+                  :
+                  <Droppable
+                  key={key}
+                  types={['img']} // <= allowed drop types
+                  onDrop={this.onDrop}>
+                  <div className="droppable" 
+                  index={key}>
+                  <div>
+                    <img 
+                    src={this.state.indexImg === key ? this.state.dragElement : ''} 
+                    alt="img"/>
+                  </div>
+                  </div>
+                  </Droppable>
+                )
+              })}
+          </div>
+        </div>
+    )
+  }
+}
+
+export default App;
